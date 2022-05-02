3730e1a19ded67a5a66523cb6622abbdd752de07
calvin <ccnlui@gmail.com>
2022-05-02 10:46:15 -0400

    echo server + client

---
+++ app/src/main/java/nemesis/svc/Nemesis.java
@@ -0,0 +1,23 @@
+package nemesis.svc;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+    name = "nemesis",
+    mixinStandardHelpOptions = true,
+    description = "nemesis nanoservices",
+    usageHelpAutoWidth = true
+)
+public class Nemesis {
+    
+    public static void main( String[] args ) {
+
+        CommandLine cmd = new CommandLine(new Nemesis());
+        cmd.addSubcommand("echoserver", EchoServer.class);
+        cmd.addSubcommand("echoclient", EchoClient.class);
+        System.exit(cmd.execute(args));
+
+    }
+
+}
