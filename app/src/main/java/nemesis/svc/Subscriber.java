6797bc402c46c030923f2eebaf296647335b6dff
calvin <ccnlui@gmail.com>
2022-05-10 15:01:25 -0400

    benchmark udp subscriber (not tuned

--- app/src/main/java/nemesis/svc/Subscriber.java
+++ app/src/main/java/nemesis/svc/Subscriber.java
@@ -1,8 +1,10 @@
 package nemesis.svc;
 
 import java.nio.ByteBuffer;
+import java.time.Instant;
 import java.util.concurrent.Callable;
 
+import org.HdrHistogram.Histogram;
 import org.agrona.concurrent.BusySpinIdleStrategy;
 import org.agrona.concurrent.IdleStrategy;
 import org.agrona.concurrent.UnsafeBuffer;
@@ -34,14 +36,21 @@ public class Subscriber implements Callable<Void>
 
     private static final Logger LOG = LoggerFactory.getLogger(Subscriber.class);
 
+    final long WARMUP_TIME_MSEC = 10_000;
+    final long RUN_TIME_MSEC    = 20_000;
+
     @Override
     public Void call() throws Exception
     {
         final UnsafeBuffer usb = new UnsafeBuffer(ByteBuffer.allocateDirect(TransmissionBlock.MAX_SIZE));
         final TransmissionBlock block = new TransmissionBlock();
+
         final String channel = "aeron:udp?endpoint=127.0.0.1:2000|mtu=1408";
         final int stream = 10;
         final IdleStrategy idleStrategyReceive = new BusySpinIdleStrategy();
+
+        long startTime  = System.currentTimeMillis();
+        Histogram scbInDelay = new Histogram(60_000_000_000L, 3);
 
         // construct media driver, clean up media driver folder on start/stop
         final MediaDriver.Context mediaDriverCtx = new MediaDriver.Context()
@@ -65,19 +74,35 @@ public class Subscriber implements Callable<Void>
                 buffer.getBytes(offset, usb, 0, length);
                 block.fromByteBuffer(usb.byteBuffer());
                 // block.parseHeader();
+                if (bench && (System.currentTimeMillis() - startTime > WARMUP_TIME_MSEC))
+                {
+                    scbInDelay.recordValue(nowNano() - block.sipBlockTimestamp());
+                }
             };
 
         while (true)
         {
             final int fragmentsRead = sub.poll(handler, 10);
+            if (bench && System.currentTimeMillis() - startTime > WARMUP_TIME_MSEC + RUN_TIME_MSEC)
+            {
+                LOG.info("---------- scbInDelay (us) ----------");
+                scbInDelay.outputPercentileDistribution(System.out, 1000.0);  // output in us
+                break;
+            }
             idleStrategyReceive.idle(fragmentsRead);
         }
 
-        // // clean up
-        // sub.close();
-        // aeron.close();
-        // mediaDriver.close();
+        // clean up
+        sub.close();
+        aeron.close();
+        mediaDriver.close();
 
-        // return null;
+        return null;
+    }
+
+    private long nowNano()
+    {
+        Instant now = Instant.now();
+        return now.getEpochSecond() * 1_000_000_000L + now.getNano();
     }
 }
