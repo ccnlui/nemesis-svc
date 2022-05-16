01cb166ea303129792fdc6bd23dc35295703b2b0
calvin <ccnlui@gmail.com>
2022-05-16 14:26:55 -0400

    babl stream server

--- app/src/main/java/nemesis/svc/BablStreamServer.java
+++ app/src/main/java/nemesis/svc/BablStreamServer.java
@@ -1,64 +1,34 @@
 package nemesis.svc;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-
-import com.aitusoftware.babl.config.BablConfig;
-import com.aitusoftware.babl.config.PropertiesLoader;
 import com.aitusoftware.babl.user.Application;
 import com.aitusoftware.babl.user.BroadcastSource;
 import com.aitusoftware.babl.user.ContentType;
-import com.aitusoftware.babl.websocket.BablServer;
 import com.aitusoftware.babl.websocket.DisconnectReason;
 import com.aitusoftware.babl.websocket.SendResult;
 import com.aitusoftware.babl.websocket.Session;
-import com.aitusoftware.babl.websocket.SessionContainers;
 import com.aitusoftware.babl.websocket.broadcast.Broadcast;
 
 import org.agrona.DirectBuffer;
 import org.agrona.ExpandableDirectByteBuffer;
 import org.agrona.MutableDirectBuffer;
-import org.agrona.concurrent.UnsafeBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.aeron.logbuffer.Header;
 
 public class BablStreamServer implements Application, BroadcastSource
 {
     private static Logger LOG = LoggerFactory.getLogger(BablStreamServer.class);
+    private static int BROADCAST_TOPIC = 42;
 
     private final MutableDirectBuffer buffer = new ExpandableDirectByteBuffer(512);
-    private Broadcast broadcast;
-
-    public static void main(String[] args) throws InterruptedException
-    {
-        String configPath = "/Users/calvin/source/java/nemesis-svc/babl-performance.properties";
-        final BablConfig config = PropertiesLoader
-            .configure(Paths.get(configPath));
-        BablStreamServer streamServer = new BablStreamServer();
-        config.applicationConfig().application(streamServer);
-        try (SessionContainers containers = BablServer.launch(config))
-        {
-            containers.start();
-
-            streamServer.broadcast.createTopic(42);
-            final String TOPIC_ONE_MSG = "TOPIC_ONE";
-            final UnsafeBuffer topicOneMsg = new UnsafeBuffer(TOPIC_ONE_MSG.getBytes(StandardCharsets.UTF_8));
-
-            while (true)
-            {
-                streamServer.broadcast.sendToTopic(42, ContentType.TEXT, topicOneMsg, 0, topicOneMsg.capacity());
-                Thread.sleep(1);
-            }
-
-            // new ShutdownSignalBarrier().await();
-        }
-    }
+    private Broadcast broadcastHandler;
 
     @Override
     public int onSessionConnected(Session session)
     {
         LOG.info("connected: {}", session.toString());
-        broadcast.addToTopic(42, session.id());
+        broadcastHandler.addToTopic(BROADCAST_TOPIC, session.id());
         return SendResult.OK;
     }
 
@@ -69,7 +39,7 @@ public class BablStreamServer implements Application, BroadcastSource
             session.toString(),
             reason.toString()
         );
-        broadcast.removeFromTopic(42, session.id());
+        broadcastHandler.removeFromTopic(BROADCAST_TOPIC, session.id());
         return SendResult.OK;
     }
 
@@ -94,6 +64,16 @@ public class BablStreamServer implements Application, BroadcastSource
     @Override
     public void setBroadcast(Broadcast broadcast)
     {
-        this.broadcast = broadcast;
+        this.broadcastHandler = broadcast;
+    }
+
+    public void createBroadcastTopic()
+    {
+        this.broadcastHandler.createTopic(BROADCAST_TOPIC);
+    }
+
+    public int broadcast(DirectBuffer buffer, int offset, int length, Header header)
+    {
+        return this.broadcastHandler.sendToTopic(BROADCAST_TOPIC, ContentType.TEXT, buffer, offset, length);
     }
 }
--- app/src/main/java/nemesis/svc/Broadcaster.java
+++ app/src/main/java/nemesis/svc/Broadcaster.java
@@ -1,12 +1,21 @@
 package nemesis.svc;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
+
+import com.aitusoftware.babl.config.BablConfig;
+import com.aitusoftware.babl.config.PropertiesLoader;
+import com.aitusoftware.babl.user.ContentType;
+import com.aitusoftware.babl.websocket.BablServer;
+import com.aitusoftware.babl.websocket.SessionContainers;
 
 import org.agrona.concurrent.AgentRunner;
 import org.agrona.concurrent.BusySpinIdleStrategy;
 import org.agrona.concurrent.IdleStrategy;
 import org.agrona.concurrent.NoOpIdleStrategy;
 import org.agrona.concurrent.ShutdownSignalBarrier;
+import org.agrona.concurrent.UnsafeBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +23,7 @@ import io.aeron.Aeron;
 import io.aeron.Subscription;
 import io.aeron.driver.MediaDriver;
 import io.aeron.driver.ThreadingMode;
+import nemesis.svc.agent.BablBroadcastAgent;
 import nemesis.svc.agent.BroadcastAgent;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -38,6 +48,10 @@ public class Broadcaster implements Callable<Void>
         description = "websocket server port (default: ${DEFAULT-VALUE})")
     int port;
 
+    @Option(names = "--websocket-lib", defaultValue = "${WEBSOCKET_LIB:-babl}",
+        description = "websocket server library (default: ${DEFAULT-VALUE})")
+    String websocketLib;
+
     private static final Logger LOG = LoggerFactory.getLogger(Broadcaster.class);
 
     @Override
@@ -55,26 +69,59 @@ public class Broadcaster implements Callable<Void>
         // construct publication and subscription
         final Subscription sub = aeron.addSubscription(inChannel, inStream);
 
-        final StreamServer streamServer = new StreamServer(port);
+        
+        AgentRunner agentRunner = null;
+        SessionContainers containers = null;
+        switch (websocketLib)
+        {
+            case "babl" ->
+            {
+                // construct babl server
+                final String configPath = "/home/calvin/source/java/nemesis-svc/babl-performance.properties";
+                final BablConfig config = PropertiesLoader.configure(Paths.get(configPath));
+                final BablStreamServer bablStreamServer = new BablStreamServer();
 
-        // construct the agents
-        final BroadcastAgent marshalAgent = new BroadcastAgent(sub, streamServer);
-        final AgentRunner agentRunner = new AgentRunner(
-            idleStrategy,
-            Throwable::printStackTrace,
-            null,
-            marshalAgent
-        );
+                // construct the agents
+                final BablBroadcastAgent bablBroadcastAgent = new BablBroadcastAgent(sub, bablStreamServer);
+                agentRunner = new AgentRunner(
+                    idleStrategy,
+                    Throwable::printStackTrace,
+                    null,
+                    bablBroadcastAgent
+                );
 
-        LOG.info("starting marshaller");
-        streamServer.start();
-        AgentRunner.startOnThread(agentRunner);
+                LOG.info("starting babl broadcaster");
+                containers = BablServer.launch(config);
+                containers.start();
+                AgentRunner.startOnThread(agentRunner);
+            }
+                
+            case "java-websocket" ->
+            {
+                // construct java-websocket server
+                final StreamServer streamServer = new StreamServer(port);
+
+                // construct the agents
+                final BroadcastAgent broadcastAgent = new BroadcastAgent(sub, streamServer);
+                agentRunner = new AgentRunner(
+                    idleStrategy,
+                    Throwable::printStackTrace,
+                    null,
+                    broadcastAgent
+                );
+
+                LOG.info("starting java-websocket broadcaster");
+                streamServer.start();
+                AgentRunner.startOnThread(agentRunner);
+            }
+        }
 
         // wait for the shutdown signal
         barrier.await();
 
         // close the resources
         closeIfNotNull(agentRunner);
+        closeIfNotNull(containers);
         closeIfNotNull(aeron);
         closeIfNotNull(mediaDriver);
 
